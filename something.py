ccd0b3ad8b57ba2f32ff6995c2aa24c8d3fa7a0d
axhero7 <axhero7@gmail.com>
2024-05-25 19:05:33 -0700

    Create something.py

---
+++ something.py
@@ -0,0 +1,14 @@
+import requests
+import json
+
+def get_epub(dir):
+    return [file for file in os.listdir(dir) if file.endswith('.epub') ]
+url = 'https://api.jsonbin.io/v3/b/665295efe41b4d34e4f96e99'
+headers = {
+  'Content-Type': 'application/json',
+  'X-Master-Key': '$2a$10$Sxm35XHa2DxDymTpdpjws.WJIghRt6HO3BcnHMUvsKxGaRfvTOm36'
+}
+for file in get_epub('.'):
+  data = json.loads(open(file, 'r').read())
+  req = requests.put(url, json=data, headers=headers)
+  print(file)
